669326adb22e38c3219d37100e5c20474b4d085a
DavidBSTRN <davidbystron172@gmail.com>
2023-12-04 19:47:13 +0100

    Binar nums are copyable

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ ctk.set_default_color_theme("dark-blue")
 # main
 app = ctk.CTk()
 app.title("CRC encode and decode")
-app.geometry("800x480")
+app.resizable(False, False)
 
 # tabview
-tabview = ctk.CTkTabview(app, width = 780, height = 460)
+tabview = ctk.CTkTabview(app)
 tabview.pack()
 
 # my tabs
@@ -28,11 +28,11 @@ def get_poly():
 
         gen_poly = ' | '.join(crc.find_gen_poly(nk[0], nk[1]))
 
-        gen_label.configure(text = f"Gen. pol ({nk[0]},{nk[1]}): {gen_poly}")
+        gen_text.set(f"Gen. pol: {gen_poly}")
         nk_label.configure(text="")
     except:
-        gen_label.configure(text="")
-        nk_label.configure(text = "Input the 'n,k'...(7,4)")
+        gen_text.set("")
+        nk_label.configure(text="Input the 'n,k'...7,4")
 
 def encode_msg():
     message = msg_enter.get()
@@ -40,23 +40,23 @@ def encode_msg():
 
     code_message = crc.encoder(message, key)
 
-    bin_msg.configure(text = f"{code_message}")
-    poly_msg.configure(text = f"{crc.bin_to_poly(code_message)}")
+    bin_text.set(f"{code_message}")
+    poly_msg.configure(text=f"{crc.bin_to_poly(code_message)}")
 
 def decode_msg():
     message = msg_enter2.get()
     key = gen_enter2.get()
 
     if int(crc.mod_2(message, key)) == 0:
-        decode_label.configure(text = "Message is correct.")
-        correct_label.configure(text = f"{message}")
-        poly_label.configure(text = f"{crc.bin_to_poly(message)}")
+        decode_label.configure(text="Message is correct.")
+        correct_text.set(f"{message}")
+        poly_label.configure(text=f"{crc.bin_to_poly(message)}")
     else:
         correct_message = ' | '.join(crc.find_mistake(message, key))
 
         decode_label.configure(text="Message is incorrect.")
-        correct_label.configure(text = f"Possible correct message: {correct_message}")
-        poly_label.configure(text="To show polynomial, correct the message")
+        correct_text.set(f"Correction: {correct_message}")
+        poly_label.configure(text="")
 
 # ENCODE
 # entry 'n,k'
@@ -68,7 +68,10 @@ nk_label = ctk.CTkLabel(encode_tab, text = "")
 nk_label.grid(row = 1, column = 0, padx = 10, pady = (10,0))
 
 # gen polynomials label
-gen_label = ctk.CTkLabel(encode_tab, text = "")
+gen_text = ctk.StringVar()
+gen_text.set("")
+gen_label = ctk.CTkEntry(encode_tab, fg_color= "transparent", state = "readonly", border_color="gray13",
+                        textvariable = gen_text, width = 220, justify = "center")
 gen_label.grid(row = 0, column = 2, padx = 10, pady = (10,0))
 
 # Gen button
@@ -88,10 +91,13 @@ encode_button = ctk.CTkButton(encode_tab, text = "Encode", command = encode_msg)
 encode_button.grid(row = 2, column = 1, padx = 10, pady = (10,0))
 
 # Code message labels
-bin_msg = ctk.CTkLabel(encode_tab, text = "")
+bin_text = ctk.StringVar()
+bin_text.set("")
+bin_msg = ctk.CTkEntry(encode_tab, fg_color= "transparent", state = "readonly", border_color="gray13",
+                        textvariable = bin_text, width = 220, justify = "center")
 bin_msg.grid(row = 2, column = 2, padx = 10, pady = (10,0))
 
-poly_msg = ctk.CTkLabel(encode_tab, text = "")
+poly_msg = ctk.CTkLabel(encode_tab, text = "", anchor = "w")
 poly_msg.grid(row = 3, column = 2, padx = 10, pady = (10,0))
 
 # DECODE
@@ -108,13 +114,16 @@ decode_button = ctk.CTkButton(decode_tab, text = "Decode", command = decode_msg)
 decode_button.grid(row = 0, column = 1, padx = 10, pady = (10,0))
 
 # Decode label
-decode_label = ctk.CTkLabel(decode_tab, text = "")
+decode_label = ctk.CTkLabel(decode_tab, text = "", anchor = "w")
 decode_label.grid(row = 0, column = 2, padx = 10, pady = (10,0))
 
-correct_label = ctk.CTkLabel(decode_tab, text = "")
+correct_text = ctk.StringVar()
+correct_text.set("")
+correct_label = ctk.CTkEntry(decode_tab, fg_color= "transparent", state = "readonly", border_color="gray13",
+                        textvariable = correct_text, width = 220, justify = "center")
 correct_label.grid(row = 1, column = 2, padx = 10, pady = (10,0))
 
-poly_label = ctk.CTkLabel(decode_tab, text = "")
+poly_label = ctk.CTkLabel(decode_tab, text = "", anchor = "w")
 poly_label.grid(row = 2, column = 2, padx = 10, pady = (10,0))
 
 app.mainloop()
